3bcf7f71f3e18937315f6cc7a642feb394c8ada0
James Chiu Wai Chun <jospehy6@gmail.com>
2023-04-25 18:09:50 +0100

    Modified upload image aspect in sign.js, sign.css and sign.html

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,18 +1,37 @@
 // Sign up btn
 document
-	.querySelector('#sign-up-form')
-	.addEventListener('submit', async (e) => {
-		e.preventDefault();
+    .querySelector('#sign-up-form')
+    .addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-		const form = e.target;
-		const formData = new FormData(form);
+        const form = e.target;
+        const formData = new FormData(form);
 
-		const res = await fetch('/sign', {
-			method: 'POST',
-			body: formData
-		});
-		form.reset();
+        const res = await fetch('/sign', {
+            method: 'POST',
+            body: formData
+        });
+        form.reset();
 
-		const result = await res.json();
-		console.log(result.success);
-	});
+        const result = await res.json();
+        console.log(result.success);
+    });
+
+
+// image upload functionality
+const imgDiv = document.querySelector('.profile-pic-div')
+const img = document.querySelector('.profile-pic')
+const file = document.querySelector('#image')
+const uploadBtn = document.querySelector('.image-upload-btn')
+
+// Change profile img display instantly
+file.addEventListener('change', (e)=>{
+    const [newImg] = e.target.files
+    if (newImg){
+        const reader = new FileReader()
+        reader.addEventListener('load', ()=>{
+            img.setAttribute('src', reader.result)
+        })
+        reader.readAsDataURL(newImg)
+    }
+})
