8377d4a7e2c883ba79a31b1f09c2aa62d40f3764
James Chiu Wai Chun <jospehy6@gmail.com>
2023-04-25 18:35:10 +0100

    Created hash.ts using bcrypt

---
+++ hash.ts
@@ -0,0 +1,18 @@
+import * as bcrypt from "bcryptjs";
+
+// Configure bcryptjs
+const SALT_ROUNDS = 10;
+
+export async function hashPassword(plainPassword: string) {
+    const hash = await bcrypt.hash(plainPassword, SALT_ROUNDS);
+    return hash
+}
+
+export async function checkPassword(
+    plainPassword: string,
+    hashPassword: string
+) {
+    const isMatched: boolean = await bcrypt.compare(plainPassword, hashPassword)
+    return isMatched
+}
+
