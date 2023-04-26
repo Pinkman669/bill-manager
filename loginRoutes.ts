e333de6c21cd670f13d958d2e315fb7ea48ab72c
Naomi Chan <channaomi.b@gmail.com>
2023-04-26 09:51:03 -0400

    created login page

--- loginRoutes.ts
+++ loginRoutes.ts
@@ -5,6 +5,8 @@ import { logger } from './logger';
 import { form } from './main'
 import formidable from 'formidable';
 import { hashPassword, checkPassword } from './hash';
+import {User} from "./type";
+import crypto from 'crypto';
 
 export const loginRoutes = express.Router();
 
@@ -40,6 +42,74 @@ export async function signIn(req: Request, res: Response) {
 	}
 }
 
+//login 
 
+loginRoutes.post("/login" ,async(req, res) =>{
+	const {email, password} = req.body;
+    const result = await client.query ("Select * from users where password = $1 and email = $2", [password, email])
+    const users: User[] = result.rows;
+	if(users.length){
+		//login success
+		req.session.user = email
+		console.log("login success");
+        res.json({success: true});
+	}else{
+        console.log("login fail");
+        res.json({success: false});
+        throw new Error("Username/")
+    }
+})
+
+loginRoutes.get('/login/google', async (req:express.Request, res:express.Response) =>{
+    const accessToken = req.session?.['grant'].response.access_token;
+
+    const fetchRes = await fetch('https://www.googleapis.com/oauth2/v2/userinfo',{
+        method:"get",
+        headers:{
+            "Authorization":`Bearer ${accessToken}`
+        }
+    })
+    const result = await fetchRes.json()
+    const users = (await client.query(
+        `SELECT * FROM users WHERE users.username = $1`, [result.email])).rows;
+
+    let user = users[0];
+    if(!user){
+        user = ( await client.query(
+                `INSERT INTO users (email,password)
+                VALUES ($1,$2) RETURNING *`,
+                [result.email, await hashPassword(crypto.randomBytes(20).toString())])
+            ).rows[0]
+    }
+
+    if(req.session){
+        req.session['user'] = user.email
+        req.session['userID'] = user.id
+        };
+        res.redirect('/landing')
+    }
+)
+
+
+loginRoutes.get('/logout',(req,res)=>{
+    if(req.session){
+        delete req.session['user']
+        delete req.session['email']
+        delete req.session['password']
+    }
+})
+
+export const isLoggedIn = (
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction
+  ) => {
+	if (req.session .user) {
+	  next ();
+	} else {
+		// login fail
+	  res.redirect("/login.html")
+	}
+  };
 
 checkPassword; // placeholder only
--- main.ts
+++ main.ts
@@ -6,13 +6,18 @@ import pg from 'pg';
 import formidable from 'formidable';
 import fs from 'fs';
 import { loginRoutes } from './loginRoutes';
+<<<<<<< HEAD
 import path from 'path'
 import {memberRoutes} from './memberRoutes'
+=======
+import { isLoggedIn } from './loginRoutes';
+import grant from 'grant';
+>>>>>>> 2d387f8 (created login page)
 
 declare module 'express-session' {
 	interface SessionData {
 		user?: string;
-		userID?: number;
+		userID?: number;		
 	}
 }
 
@@ -37,6 +42,23 @@ app.use(
 		saveUninitialized: true
 	})
 );
+// google login 
+const grantExpress = grant.express({
+	defaults: {
+		origin: "http://localhost:8080",
+		transport: "session",
+		state: true,
+	  },
+	  google: {
+		key: process.env.GOOGLE_CLIENT_ID || "",
+		secret: process.env.GOOGLE_CLIENT_SECRET || "",
+		scope: ["profile", "email", "openid"],
+		callback: "/login/google",
+	  },
+	});
+	
+	app.use(grantExpress as express.RequestHandler);
+
 
 // Configure formidable
 const uploadDir = 'public/uploads';
@@ -70,6 +92,9 @@ export function isLoggedIn(req: express.Request, res: express.Response, next: ex
 app.use((req: express.Request, res: express.Response) => {
 	res.sendFile(path.resolve('./public/404.html'));
 });
+
+// admin.html should be inside protected
+app.use(isLoggedIn, express.static("protected"));
 
 const PORT = 8080;
 app.listen(PORT, () => {
