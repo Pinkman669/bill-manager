7f91e980f6204c7f2a4a847f6f928f2221340d29
Naomi Chan <channaomi.b@gmail.com>
2023-04-26 17:09:20 -0400

    rearrange code order in main.ts

--- main.ts
+++ main.ts
@@ -85,13 +85,13 @@ app.use('/member', memberRoutes); // routes about member function
 // }
 
 
+// admin.html should be inside protected
+app.use(isLoggedIn, express.static("protected"));
+
 // Redirecting
 app.use((req: express.Request, res: express.Response) => {
 	res.sendFile(path.resolve('./public/404.html'));
 });
-
-// admin.html should be inside protected
-app.use(isLoggedIn, express.static("protected"));
 
 const PORT = 8080;
 app.listen(PORT, () => {
