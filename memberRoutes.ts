37a12a6317fa061ee31c4ae04275fa1f4a51fdc7
James Chiu Wai Chun <jospehy6@gmail.com>
2023-04-26 19:37:50 +0100

    Added owe table in member page

--- memberRoutes.ts
+++ memberRoutes.ts
@@ -8,16 +8,25 @@ export const memberRoutes = express.Router()
 memberRoutes.get('/', getMember) // Member home page
 
 
-export async function getMember(req: Request, res: Response){
-    try{
+export async function getMember(req: Request, res: Response) {
+    try {
         req.session.userID = 30 // Temp use only
         const userID: number = req.session.userID
-        const userInfo = await client.query('SELECT * FROM users WHERE id = $1',[
+        const userInfo = await client.query(`SELECT nickname, image FROM users WHERE id = $1`, [
             userID
         ])
-        res.json(userInfo.rows[0])
-    } catch (e){
+
+        const requestorInfo = await client.query(`SELECT DISTINCT events.date,records.event_id,events.amount, users.nickname, users.id 
+        FROM records INNER JOIN events ON records.requestor_id = events.user_id 
+        INNER JOIN users ON users.id = events.user_id 
+        WHERE records.receiver_id = $1 ORDER BY events.date`, [
+            userID
+        ])
+
+        
+        res.json({ user: userInfo.rows, requestor: requestorInfo.rows })
+    } catch (e) {
         logger.error('[Err002] User not found' + e)
-        res.json({success: false, msg: '[ERR002]'})
+        res.json({ success: false, msg: '[ERR002]' })
     }
 }
