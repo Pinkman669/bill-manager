addb0aef0067200d1b3242e25f452e9364616b39
Nicole Lai <nicolellh1123@gmail.com>
2023-05-04 11:34:17 -0400

    Testing Friends List

--- activityRoutes.ts
+++ activityRoutes.ts
@@ -1,8 +1,15 @@
-import express from 'express';
-
-
+import express from 'express'
+import {Request, Response} from 'express'
+import { client } from './main';
+import { isLoggedIn } from './loginRoutes';
 
 export const activityRoutes = express.Router();
+
+// get the friends list
+activityRoutes.get('/', (req: Request, res: Response) => {
+    const userID = req.session.userID;
+    const friends = client.query(`SELECT user1_id FROM friends INNER JOIN users ON friends.id = users.id`.[userID])
+})
 
 
 
