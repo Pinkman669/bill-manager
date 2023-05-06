1081d57f35fac4ffd4348733b605b07dfd8aeef8
Naomi Chan <channaomi.b@gmail.com>
2023-05-06 15:37:24 -0400

    added detail in event detail page

--- eventDetail.ts
+++ eventDetail.ts
@@ -1,4 +1,4 @@
-import express, { query } from 'express';
+import express from 'express';
 import { Request, Response } from 'express';
 import { client } from './main';
 import { logger } from './logger';
@@ -9,20 +9,52 @@ eventDetail.get('/:recordId', isLoggedIn, getEventDetail);
 
 export async function getEventDetail(req: Request, res: Response) {
 	try {
-        const  recordId= req.params.recordId;
-       
+        const recordId= req.params.recordId;
+      
+        const userID = req.session.userID;
+
+        const userInfo = await client.query(
+			`SELECT nickname FROM users WHERE id = $1`,
+			[userID]
+        )
+
         const eventDetail = await client.query(
 			`SELECT 
-            events.id as event_id, events.name, events.date, records.amount, records.requestor_id, records.receiver_id, events.split-method, events.amount,records.due,
+            events.id as event_id, events.name, events.date, records.amount as record_amount, records.requestor_id, records.receiver_id, events.method , events.msg as message, events.amount as total_amount,records.due
             FROM 
             records INNER JOIN events ON events.id =records.event_id
             WHERE 
             records.id= $1`,
 			[recordId]
         );
+        const requestor = await client.query(
+            `SELECT
+            users.id,users.nickname, records.requestor_id
+            FROM
+            records INNER JOIN users ON users.id = records.requestor_id
+            WHERE 
+            records.id= $1`,
+			[recordId]
+        )
+        const receiver = await client.query(
+            `SELECT
+            users.id,users.nickname, records.receiver_id
+            FROM
+            records INNER JOIN users ON users.id = records.receiver_id 
+            WHERE 
+            records.id= $1`,
+			[recordId]
+        )
 
 
-        res.json({eventinfo: eventDetail})
+        res.json({
+            user: userInfo.rows,
+            req: requestor.rows,
+            res: receiver.rows,
+            eventInfo: eventDetail.rows
+        })
+        // console.log(JSON.stringify({user: userInfo,req: requestor,res: receiver, eventInfo: eventDetail}));
+
 
 	} catch (e) {
 		logger.error('[Err006] Event detail not Found ' + e);
--- public/js/event-detail.js
+++ public/js/event-detail.js
@@ -1,8 +1,29 @@
 const searchParams = new URLSearchParams(location.search);
 const id = searchParams.get("recordId"); 
 
-async function loadFriendsDetail() {
+async function loadEventDetail() {
 	const res = await fetch(`/eventdetail/${id}`);
 	const result = await res.json();
+	const eventName = document.querySelector('#event-name');
+	const eventDate = document.querySelector('#event-date');
+	const ownAmount = document.querySelector('#own-amount')
     const splitMethod = document.querySelector('#split-method');
+	const friendsInvolved = document.querySelector('#friends-involved');
+	const billTotal = document.querySelector('#bill-total-amount');
+	const message = document.querySelector('#message');
+
+	
+	eventName.textContent = result.eventInfo[0].name;
+	eventDate.textContent = new Date(result.eventInfo[0].date).toDateString();
+	ownAmount.textContent = result.eventInfo[0].record_amount;
+	splitMethod.textContent = result.eventInfo[0].Method;
+	friendsInvolved.textContent = `${result.req[0].nickname}+ +${result.res[0].nickname}`;
+	billTotal.textContent = result.eventInfo[0].total_amount;
+	message.textContent = result.eventInfo[0].message;
+
 }
+
+// Window onload function
+window.addEventListener('load', async () => {
+	loadEventDetail();
+});
